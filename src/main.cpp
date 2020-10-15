844d0dd4f250b724361ac690c8060c36b32ee85a
XWasHere <70487315+XWasHere@users.noreply.github.com>
2020-10-15 12:52:37 -0400

    add basic argument stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <string.h>
+
 using namespace std;
-int Main(int argc, char** argv) {
-  cout << argv[1];
+
+void showHelp() {
+cout << "Usage: InvertedScript [-h | -s (script)]" << endl;
+}
+
+void runFile(char*) {
+
+}
+
+void runPrompt() {
+
+}
+int main(int argc, char* argv[]) {
+if (_stricmp(argv[1], "-h") == 0) {
+showHelp(); //show the help thing
+return 0;
+} else if (_stricmp(argv[1], "/h") == 0) {
+cout << "I understand your confusion fellow Windows user, But please start command line flags UNIX style, for example you could type -h to get help" << endl;
+} else if (_stricmp(argv[1], "-s") == 0) {
+runFile(argv[1]);
+} else {
+runPrompt();
+}
 }
