9c5261d7be233fd6b551dfdf4a01edc6c8a42f16
X-San <70487315+XWasHere@users.noreply.github.com>
2020-10-20 18:11:39 -0400

    i said commit.

--- src/table.h
+++ src/table.h
@@ -15,4 +15,13 @@ typedef struct {
   Entry* entries;
 } Table;
 
+void initTable(Table* table);
+void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);
+ObjString* tableFindString(Table* table, const char* chars,
+                           int length, uint32_t hash);
+
 #endif
